cd711b32b6be2e29578fb9451f4b08c53ab3abb5
tanmaysayare <tanmaysayare12345@gmail.com>
2024-05-21 23:46:43 +0530

    re - rendering completed

--- part1/pract-rv/src/App.jsx
+++ part1/pract-rv/src/App.jsx
@@ -1,29 +1,10 @@
-const App = () => {
-  const friends = [
-    {name:'Tanmay' , age : 20},
-    {name:'Sultan' , age : 38},
-  ]
+const App = (props) => {
+  const {counter} = props
   return (
     <div>
-      <Friend  friends={friends}/>
-    </div>
+      <h1> Counter is {counter}</h1>
+      </div>
   )
-}
-
-const Diff = ({friend}) => {
-  return(
-    <div>
-      <h1>My name is {friend.name} and my age is {friend.age}</h1>
-    </div>
-  )
-}
-const Friend = ({friends}) => {
-  return (
-    <div>
-      <Diff friend={friends[0]}/>
-      <Diff friend={friends[1]}/>
-    </div>
-  )  
 }
 
 export default App
