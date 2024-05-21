1bc84b70658087574c0a92abcf877f58de8040e0
tanmaysayare <tanmaysayare12345@gmail.com>
2024-05-21 23:25:57 +0530

    Destructing Completed

--- part1/pract-rv/src/App.jsx
+++ part1/pract-rv/src/App.jsx
@@ -10,18 +10,18 @@ const App = () => {
   )
 }
 
-const Diff = (props) => {
+const Diff = ({friend}) => {
   return(
     <div>
-      <h1>My name is {props.friends.name} and my age is {props.friends.age}</h1>
+      <h1>My name is {friend.name} and my age is {friend.age}</h1>
     </div>
   )
 }
-const Friend = (props) => {
+const Friend = ({friends}) => {
   return (
     <div>
-      <Diff friends={props.friends[0]}/>
-      <Diff friends={props.friends[1]}/>
+      <Diff friend={friends[0]}/>
+      <Diff friend={friends[1]}/>
     </div>
   )  
 }
