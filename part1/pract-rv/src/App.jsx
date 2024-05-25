1c2d9d45aa4cfe0638e66402817fea156ad7c6c9
tanmaysayare <tanmaysayare12345@gmail.com>
2024-05-25 21:47:52 +0530

    Event handling

--- part1/pract-rv/src/App.jsx
+++ part1/pract-rv/src/App.jsx
@@ -4,9 +4,8 @@ const App = () => {
 
   const [ counter, setCounter ] = useState(0)
 
-  console.log('rendering...', counter)
 
-  if(counter != 20){
+  /*if(counter != 20){
   setTimeout(
     () => setCounter(counter + 1),
     1000
@@ -18,8 +17,49 @@ const App = () => {
     )// reset the counter to 0;
   }
 
+  console.log('rendering...', counter)*/
+
+  const inc = () => {
+    if(counter != 10){
+      setTimeout(
+        () => setCounter(counter + 1),
+      )}
+      else{
+        setTimeout(
+          () => setCounter(0),
+        )// reset the counter to 0;
+      }
+    console.log('Inceremented')
+  }
+
+  const dec = () => {
+    if(counter > -10){
+    setTimeout(
+      () => setCounter(counter - 1),
+    )
+  }else{
+    setTimeout(
+      () => setCounter(0),
+    )// reset the counter to 0;
+  }
+    console.log('Decremented')
+  }
+
+  console.log('Counter value = ', counter)
+
   return (
-    <div><h1>Counter is {counter}</h1></div>
+    <div>
+      <h1>Counter = {counter}</h1>
+      <button onClick={inc}>
+        INC
+      </button>
+      <button onClick={dec}>
+        DEC
+      </button>
+      <button onClick={() => setCounter(0)}>
+        ZERO
+      </button>
+    </div>
   )
 }
 
