fe2898598377cbc801422a80a580f61b4fe8c3e3
tanmaysayare <tanmaysayare12345@gmail.com>
2024-06-01 01:29:33 +0530

    unicafe step 3-4 completed

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,11 +1,41 @@
 import { useState } from 'react'
 
+
+const Button = ({ onClick, text }) => {
+  return (
+    <button onClick={onClick}>
+      {text}
+    </button>
+  )
+}
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
+  const Statistics = ({good , neutral , bad }) => {
+    const total = good + neutral + bad  // Calculate the total number of feedback
+    const average = (good - bad) / total  // Calculate the average score
+    const positivePercentage =  (good / total) * 100 // Calculate the percentage of positive feedback
+  
+    if(total === 0){
+      return <p>No feedback given</p>
+    }
+    return (
+      <div>
+            <p>good {good}</p>
+            <p>neutral {neutral}</p>
+            <p>bad {bad}</p>
+            <p>all {total}</p>
+            <p>average {average.toFixed(2)}</p>
+            <p>positive {positivePercentage.toFixed(2)} %</p>
+      </div>
+    )
+  
+  }
+  
   return (
     <div>
       <h1>give feedback</h1>
@@ -15,41 +45,11 @@ const App = () => {
         <Button onClick={() => setBad(bad + 1)} text='bad' />
       </p>
       <h1>statistics</h1>
-      <Calculation good={good} neutral={neutral} bad={bad} setBad={setBad} setGood={setGood} setNeutral={setNeutral} />
+      <Statistics good={good} neutral={neutral} bad={bad} setBad={setBad} setGood={setGood} setNeutral={setNeutral} />
     </div>
   )
 }
-const Calculation = ({good , neutral , bad }) => {
-  // Calculate the total number of feedback
-  const total = good + neutral + bad
 
-  // Calculate the average score
-  const average = (good - bad) / total
 
-  // Calculate the percentage of positive feedback
-  const positivePercentage =  (good / total) * 100
-
-  if(total === 0){
-    return <p>No feedback given</p>
-  }
-  return (
-    <div>
-          <p>good {good}</p>
-          <p>neutral {neutral}</p>
-          <p>bad {bad}</p>
-          <p>all {total}</p>
-          <p>average {average.toFixed(2)}</p>
-          <p>positive {positivePercentage.toFixed(2)} %</p>
-    </div>
-  )
-
-}
-const Button = ({ onClick, text }) => {
-  return (
-    <button onClick={onClick}>
-      {text}
-    </button>
-  )
-}
 
 export default App
