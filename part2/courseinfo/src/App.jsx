8a119e58c99eca8ba2b8acf0ab56080347ec7c4b
tanmaysayare <tanmaysayare12345@gmail.com>
2024-06-10 01:57:04 +0530

    Done Ex : 2.5: Separate module step10

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -1,3 +1,4 @@
+import Course from "./components/Course";
 const App = () => {
    const course = [
     {
@@ -45,54 +46,10 @@ const App = () => {
   ]
   return (
     <div>
-      <Course course={course[0]}/>
-      <Course course={course[1]}/>
+      <h1>Web development curriculum</h1>
+      {course.map(course => <Course key={course.id} course={course}/>)}
     </div>
   );
 }
-const Course = ({ course }) => { 
-  return (
-    <div>
-      <Header course={course.name}/>
-      <Content parts={course.parts}/>
-      <Total parts={course.parts}/>
-    </div>
-  );
-}
-
-const Header = (props) => {
-  return (
-    <div>
-      <h2>{props.course}</h2>
-    </div>
-  );
-};
-
-const Content = ({parts}) => {
-  return (
-    <div>
-      {parts.map(part => <Part key={part.id} part={part.name} ex={part.exercises}/>)}
-    </div>
-  );
-};
-
-const Part = ({part , ex}) => {
-  return (
-    <div>
-      <p>{part} {ex}</p>
-    </div>
-  );
-};
-
-const Total = ({parts}) => {
-  const total = parts.reduce((sum, part) => sum + part.exercises, 0);
-  console.log(total);
-  return (
-    <div>
-      <p><strong>total of {total} exercises </strong></p>
-    </div>
-  );
-}
-
 
 export default App;
