80d9136155d198a63004527f78ec17a6ba5c989a
tanmaysayare <tanmaysayare12345@gmail.com>
2024-06-10 01:49:19 +0530

    Done Ex : 2.3*: Course information step 8

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -62,8 +62,8 @@ const Part = ({part , ex}) => {
 };
 
 const Total = ({parts}) => {
-  let total = 0
-  parts.map(part => total += part.exercises)
+  const total = parts.reduce((sum, part) => sum + part.exercises, 0);
+  console.log(total);
   return (
     <div>
       <p><strong>total of {total} exercises </strong></p>
