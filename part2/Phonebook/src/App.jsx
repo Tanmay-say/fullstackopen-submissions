73ac24cf84e104ad7d451588700acdbbf7def83e
tanmaysayare <tanmaysayare12345@gmail.com>
2024-06-12 01:12:26 +0530

    Done 2.10: The Phonebook Step 5

--- part2/Phonebook/src/App.jsx
+++ part2/Phonebook/src/App.jsx
@@ -6,12 +6,12 @@ const App = () => {
     { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
     { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
     { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])                                                   // persons state
-  const [newName, setNewName] = useState('');         // name state
-  const [newNumber, setNewNumber] = useState('');    // number state
-  const [newFilter, setNewFilter] = useState('');   // filter state
+  ]);
+  const [newName, setNewName] = useState('');
+  const [newNumber, setNewNumber] = useState('');
+  const [newFilter, setNewFilter] = useState('');
 
-  const addPerson = (event) => {                   // addPerson function
+  const addPerson = (event) => {
     event.preventDefault();
     const personObject = {
       name: newName,
@@ -19,7 +19,6 @@ const App = () => {
       number: newNumber
     };
 
-    //Existing name and number check
     const nameExists = persons.some(person => person.name === newName);    
     const numberExists = persons.some(person => person.number === newNumber);
     if (nameExists) {
@@ -34,41 +33,63 @@ const App = () => {
   };
 
   const personsToShow = newFilter
-  ? persons.filter(person => person.name.toLowerCase().includes(newFilter.toLowerCase()))
-  : persons;
+    ? persons.filter(person => person.name.toLowerCase().includes(newFilter.toLowerCase()))
+    : persons;
 
-  const handleChange = (event) => {
-    setNewName(event.target.value);
-    setNewNumber(event.target.value);
-    setNewFilter(event.target.value);
-  }
+  const handleNameChange = (event) => setNewName(event.target.value);
+  const handleNumberChange = (event) => setNewNumber(event.target.value);
+  const handleFilterChange = (event) => setNewFilter(event.target.value);
 
   return (
     <div>
       <h2>Phonebook</h2>
-        <div>
-          filter shown with<input value={newFilter}  onChange={handleChange} />
-        </div>
-      <h2>add a new</h2>
-      <form onSubmit={addPerson}>
-        <div>
-          name: <input value={newName} onChange={handleChange} />
-        </div>
-        <div>
-          number: <input value={newNumber} onChange={handleChange} />
-        </div>
-        <div>
-          <button type="submit">add</button>
-        </div>
-      </form>
-      <h2>Numbers</h2>
-      <ul>
-        {personsToShow.map(person => (
-          <li key={person.id}>{person.name} {person.number}</li>
-        ))}
-      </ul>
+      <Filter newFilter={newFilter} handleFilterChange={handleFilterChange} />
+      <h3>Add a new</h3>
+      <PersonForm addPerson={addPerson} newName={newName} newNumber={newNumber} handleNameChange={handleNameChange} handleNumberChange={handleNumberChange} />
+      <h3>Numbers</h3>
+      <Persons persons={personsToShow} />
     </div>
   );
 };
+
+const PersonForm = ({ addPerson, newName, newNumber, handleNameChange, handleNumberChange }) => {
+  return (
+    <form onSubmit={addPerson}>
+      <div>
+        name: <input value={newName} onChange={handleNameChange} />
+      </div>
+      <div>
+        number: <input value={newNumber} onChange={handleNumberChange} />
+      </div>
+      <Button text="add" />
+    </form>
+  );
+}
+
+const Button = ({ text }) => {
+  return (
+    <div>
+      <button type="submit">{text}</button>
+    </div>
+  );
+}
+
+const Filter = ({ newFilter, handleFilterChange }) => {
+  return (
+    <div>
+      filter shown with<input value={newFilter}  onChange={handleFilterChange} />
+    </div>
+  );
+}
+
+const Persons = ({ persons }) => {
+  return(
+    <ul>
+      {persons.map(person => (
+        <li key={person.id}>{person.name} {person.number}</li>
+      ))}
+    </ul>
+  );
+}
 
 export default App;
